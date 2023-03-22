cda66164b7a121d8093df2fc6a95312c490bc2ad
Elijah <stryker1850@gmail.com>
2023-03-22 23:29:24 +0000

    added nodeList file

---
+++ final_project/Work_Jio/Work_J/nodeList.php
@@ -0,0 +1,41 @@
+
+<?php
+	function getNodeData($mysqli) {
+		$arrayMarker = array();
+		$sql="SELECT `node_id`, `node_acronym`, `node_name`, `node_region`, `pow_produce`, `pow_demand`, `node_popServe`, `node_lat`, `node_lon`, `node_connect`, `node_active`, `node_totalInflow`, `node_totalOutflow`, `node_statusPerc`  from `node_info` order by `node_region`;";
+		$result = $mysqli->query($sql) or
+			die("Something went wrong with $sql".$mysqli->error);
+		$result_Check = mysqli_num_rows($result);
+		//$output = array();
+		//var_dump($result);
+		if ($result_Check > 0) {
+            echo "HFEORG";
+			$i = 0;
+		   	while($rowData = mysqli_fetch_assoc($result)) {			
+				$arrayMarker[$i] = $rowData;
+				$i++;
+			}
+			return $arrayMarker;
+		}
+		return "N/A";	
+	}
+
+//use case: 
+    //$mysqli=db_connect("senior_design_db");
+    //$sum =  getNodeData($mysqli);//getNodeData(db_connect("senior_design_db"));
+    //returns a list of all nodes in the node_info table
+?>
+
+<?php
+    //function: get node connections, and create polylines to add to map
+    // input: map, nodeList
+    //for each node in nodeList
+        //get node latitude
+        //get node longitude
+        //access node_connect, which has a grid list of the names of the nodes it connects to
+        //for each node in the grid list:
+            //search nodeList and get node with matching name
+                // access the node longitutde
+                //  access the node latitdue  
+                //add a polyline to map with the nodes longitude and latitude 
+?>
